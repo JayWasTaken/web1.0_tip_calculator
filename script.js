1ab6b5e4e382b39b9d5438c24270756a71f78dc4
JayWasTaken <jaymay8k@gmail.com>
2020-10-06 00:44:59 -0700

    finished base challenges

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 // javaScript Code written here! 
-console.log('Hello World')
+// console.log('Hello World')
 const display = document.querySelector('#display')
-const billInput = document.querySelector('#bill')
-const tipInput = document.querySelector('#tip')
+const billInput = document.querySelector('#input-bill')
+const tipInput = document.querySelector('#input-tip')
+const peopleInput =document.querySelector('#input-people')
+const displayTip = document.querySelector('#display-tip')
+const displayTotal = document.querySelector('#display-total')
 
-console.log(display.innerHTML) // prints the text inside the tags
-console.log(billInput.value) // prints the value entered into the input
+billInput.addEventListener('input', function(e){
+    const percent = parseInt(tipInput.value)
+    const bill = parseFloat(billInput.value)
+    const people = parseInt(peopleInput.value)
+    
+    const tip = bill * (percent / 100)
+    const total = bill + tip
+})
 
-billInput.value = 999.88 // Set the value to 999.88
+
+// console.log(display.innerHTML) // prints the text inside the tags
+// console.log(billInput.value) // prints the value entered into the input
+
+billInput.value = 25.06
+tipInput.value = 15
+peopleInput.value = 3
+
+displayTip.innerHTML = tip
+displayTotal.innerHTML = total
+
+console.log(tip)
+console.log(total)
