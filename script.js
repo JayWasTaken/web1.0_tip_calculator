cb219974016a30ec0017ab4f993b2c44367b8496
JayWasTaken <jaymay8k@gmail.com>
2020-10-07 23:57:54 -0700

    finished display, added values

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-// javaScript Code written here! 
-// console.log('Hello World')
 const display = document.querySelector('#display')
 const billInput = document.querySelector('#input-bill')
 const tipInput = document.querySelector('#input-tip')
 const peopleInput =document.querySelector('#input-people')
 const displayTip = document.querySelector('#display-tip')
 const displayTotal = document.querySelector('#display-total')
+const displayResponsibility = document.querySelector('#display-responsibility')
 
-billInput.addEventListener('input', function(e){
+billInput.addEventListener('input', doMath)
+tipInput.addEventListener('input', doMath)
+peopleInput.addEventListener('input', doMath) //function(e){ //why do i add function e to this?
+    
+function doMath() {
     const percent = parseInt(tipInput.value)
     const bill = parseFloat(billInput.value)
     const people = parseInt(peopleInput.value)
     
-    const tip = bill * (percent / 100)
-    const total = bill + tip
-})
+    let tipTotal = bill * (percent / 100)
+    let tip = tipTotal / people
+    let total = bill + (tipTotal)
+    let split = total / people
 
+    displayTip.innerHTML = tip
+    displayTotal.innerHTML = total
+    displayResponsibility.innerHTML = split
+    
+    console.log(tip)
+    console.log(total)
+    console.log(split)
+}
+console.log(display.innerHTML) // prints the text inside the tags
+console.log(billInput.value) // prints the value entered into the input
 
-// console.log(display.innerHTML) // prints the text inside the tags
-// console.log(billInput.value) // prints the value entered into the input
-
-billInput.value = 25.06
+billInput.value = 20.00
 tipInput.value = 15
-peopleInput.value = 3
-
-displayTip.innerHTML = tip
-displayTotal.innerHTML = total
-
-console.log(tip)
-console.log(total)
+peopleInput.value = 1
